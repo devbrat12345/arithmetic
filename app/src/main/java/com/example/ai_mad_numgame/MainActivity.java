6b9bf711bdc0ad4e56a87d6cc7da6dfc19680012
pampaghosh2001 <105700360+pampaghosh2001@users.noreply.github.com>
2022-06-02 11:34:46 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/ai_mad_numgame/MainActivity.java
+++ app/src/main/java/com/example/ai_mad_numgame/MainActivity.java
@@ -7,7 +7,7 @@ package com.example.ai_mad_numgame;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
+import android.content.Context; 
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
